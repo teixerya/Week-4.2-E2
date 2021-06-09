ae4fd71d46f6a847308d45b7e4983be1dd03a499
Ryan Teixeira <ryan.m.tex@gmail.com>
2021-06-09 19:40:15 -0400

    added data.sql and schema.sql from Student data to test program

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO student
+(first_name, last_name, program_name, program_year, program_coop, program_internship)
+VALUES
+('Bart','Simpson','Computer Programmer',1,true,false),
+('Lisa','Simpson','Systems Technician',2,false,true),
+('Homer','Simpson','Systems Technology',1,false,false),
+('Marge','Simpson','Engineering Technician',2,true,true);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE student (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     first_name VARCHAR(30),
+     last_name VARCHAR(30),
+     program_name VARCHAR(30),
+     program_year INTEGER,
+     program_coop BOOLEAN,
+     program_internship BOOLEAN
+);
